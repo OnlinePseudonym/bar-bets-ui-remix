375b6d4d2142b9b3c4fad3327d664a7558f72d2b
= <=>
2022-07-06 11:15:34 -0700

    add some styles and stuff!

--- app/root.tsx
+++ app/root.tsx
@@ -6,27 +6,83 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
+import styles from "./styles/app.css"
+import globalStyles from "./styles/global.css"
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export function links() {
+  return [
+    { rel: "stylesheet", href: styles },
+    { rel: "stylesheet", href: globalStyles }
+  ]
+}
 
-export default function App() {
+export const meta: MetaFunction = () => {
+  const description = "A site for degenerates to track their terrible bets";
+
+  return {
+    charset: "utf-8",
+    title: "New Remix App",
+    description,
+    viewport: "width=device-width,initial-scale=1",
+  };
+}
+
+function Document({
+  children,
+  title = "BarBets: Make some bets you goon.",
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en">
       <head>
+        <title>{title}</title>
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.log(error);
+
+  return (
+    <Document title="Uh-oh!">
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <div className="error-container">
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+      </div>
+    </Document>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,19 @@
+import { LoaderFunction } from "@remix-run/node";
+
+export const loader: LoaderFunction = async () => {
+  throw new Response("No random joke found", {
+    status: 404,
+  });
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container flex flex-col justify-center align-center">
+      <div className="flex flex-col justify-center align-center py-12">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
     </div>
   );
 }
